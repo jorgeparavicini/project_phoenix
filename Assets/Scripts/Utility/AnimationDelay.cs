5d3cae25204ceabc49982e57505fd75d6fa24f78
jmparavicini <j.m.paravicini@gmail.com>
2020-06-25 15:05:23 +0200

    Baywatch animation

---
+++ Assets/Scripts/Utility/AnimationDelay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utility
+{
+    [RequireComponent(typeof(Animator))]
+    public class AnimationDelay : MonoBehaviour
+    {
+        private Animator _animator;
+        public float FirstDelay;
+        public float Delay;
+        private static readonly int Play1 = Animator.StringToHash("Play");
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            InvokeRepeating(nameof(Play), FirstDelay, Delay);
+        }
+
+        private void Play()
+        {
+            Debug.Log("Trigger");
+            _animator.SetTrigger(Play1);
+        }
+    }
+}
